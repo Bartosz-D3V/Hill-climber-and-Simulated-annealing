bb156a4e63efc5c45ce9259b50ac59ec5aa0cc91
Bartosz-D3V <nix.noxi379@gmail.com>
2017-10-29 17:16:47 +0000

    feat(sa): implemented alghoritm that can be tested
    1. Added toString methods to all relevant classes
    2. Added hashCode and equals methods to point class

--- src/domain/Point.java
+++ src/domain/Point.java
@@ -27,7 +27,7 @@ public class Point {
     }
 
     /**
-     * sqrt( (X2-X1)^2 + (Y2-Y1)2 )
+     * sqrt( (X2-X1)^2 + (Y2-Y1)^2 )
      *
      * @return
      */
@@ -39,5 +39,39 @@ public class Point {
         final double powerOf2DistanceYAxis = Math.pow(this.pointY - destPoint.pointY, 2);
 
         return Math.sqrt(powerOf2DistanceXAxis + powerOf2DistanceYAxis);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Point)) return false;
+
+        Point point = (Point) o;
+
+        if (Double.compare(point.getPointX(), getPointX()) != 0) return false;
+        return Double.compare(point.getPointY(), getPointY()) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(getPointX());
+        result = (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(getPointY());
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Position X:")
+                .append(this.pointX)
+                .append(System.lineSeparator())
+                .append("Position Y:")
+                .append(this.pointY);
+
+        return stringBuilder.toString();
     }
 }
