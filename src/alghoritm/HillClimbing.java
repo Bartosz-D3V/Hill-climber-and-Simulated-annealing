810f7832b2e28c0aeb69012f3b433a7974eaab10
Bartosz-D3V <nix.noxi379@gmail.com>
2017-11-05 12:35:17 +0000

    Added .editorconfig. Updated formatting

--- src/alghoritm/HillClimbing.java
+++ src/alghoritm/HillClimbing.java
@@ -6,43 +6,43 @@ import java.util.Random;
 
 public class HillClimbing {
 
-    private final List<Double> universe;
-    private final List<Double> notVisitedNeighbours;
+  private final List<Double> universe;
+  private final List<Double> notVisitedNeighbours;
 
-    public HillClimbing(final List<Double> universe) {
-        this.universe = universe;
-        this.notVisitedNeighbours = new ArrayList<>();
-        this.notVisitedNeighbours.addAll(universe);
-    }
+  public HillClimbing(final List<Double> universe) {
+    this.universe = universe;
+    this.notVisitedNeighbours = new ArrayList<>();
+    this.notVisitedNeighbours.addAll(universe);
+  }
 
-    public double findOptima() {
-        double initialSolution = this.universe.get(this.getRandomIndex());
-        boolean shouldContinue;
-        do {
-            double bestSolution = this.universe.get(this.getRandomIndex());
+  public double findOptima() {
+    double initialSolution = this.universe.get(this.getRandomIndex());
+    boolean shouldContinue;
+    do {
+      double bestSolution = this.universe.get(this.getRandomIndex());
 
-            // Iterate through neighbours of initialSolution
-            for (Double element : this.notVisitedNeighbours) {
-                if (element < bestSolution) {
-                    bestSolution = element;
-                }
-            }
-            if (initialSolution > bestSolution) {
-                initialSolution = bestSolution;
-                shouldContinue = true;
-            } else {
-                shouldContinue = false;
-            }
-        } while (shouldContinue);
+      // Iterate through neighbours of initialSolution
+      for (Double element : this.notVisitedNeighbours) {
+        if (element < bestSolution) {
+          bestSolution = element;
+        }
+      }
+      if (initialSolution > bestSolution) {
+        initialSolution = bestSolution;
+        shouldContinue = true;
+      } else {
+        shouldContinue = false;
+      }
+    } while (shouldContinue);
 
-        return initialSolution;
-    }
+    return initialSolution;
+  }
 
-    private int getRandomIndex() {
-        final Random random = new Random();
-        final int randomIndex = random.nextInt(this.notVisitedNeighbours.size());
-        this.notVisitedNeighbours.remove(randomIndex);
+  private int getRandomIndex() {
+    final Random random = new Random();
+    final int randomIndex = random.nextInt(this.notVisitedNeighbours.size());
+    this.notVisitedNeighbours.remove(randomIndex);
 
-        return randomIndex;
-    }
+    return randomIndex;
+  }
 }
--- src/alghoritm/SimulatedAnnealing.java
+++ src/alghoritm/SimulatedAnnealing.java
@@ -5,58 +5,58 @@ import java.util.Random;
 
 public class SimulatedAnnealing {
 
-    private final static double coolingRate = 0.004;
-    private static double temperature = 100000;
-    private final List<Double> universe;
+  private final static double coolingRate = 0.004;
+  private static double temperature = 100000;
+  private final List<Double> universe;
 
-    public SimulatedAnnealing(final List<Double> universe) {
-        this.universe = universe;
+  public SimulatedAnnealing(final List<Double> universe) {
+    this.universe = universe;
+  }
+
+  public double findOptima() {
+    // Best found solution is initially, randomly picked
+    double bestSolution = this.universe.get(this.getRandomIndex());
+    // ... and mark it as a current solution
+    double currentSolution = bestSolution;
+    // Energy of the current solution is its' value
+    double currentEnergy = currentSolution;
+    // Start annealing
+    while (temperature > coolingRate) {
+      // Get random position in our universe
+      int newSolutionPosition = this.getRandomIndex();
+      // Retrieve new solution's value
+      final double newSolutionEnergy = this.universe.get(newSolutionPosition);
+
+      // Check whether new solution should be accepted
+      if (this.acceptanceProbability(currentEnergy, newSolutionEnergy, temperature) > Math.random()) {
+        // Move on to the new solution
+        currentSolution = this.universe.get(newSolutionPosition);
+      }
+
+      // Compare to the global optima
+      if (newSolutionEnergy < bestSolution) {
+        // Mark as global optima (best found) if current global optima is greater
+        bestSolution = currentSolution;
+      }
+
+      // Cool down
+      temperature *= 1 - coolingRate;
     }
 
-    public double findOptima() {
-        // Best found solution is initially, randomly picked
-        double bestSolution = this.universe.get(this.getRandomIndex());
-        // ... and mark it as a current solution
-        double currentSolution = bestSolution;
-        // Energy of the current solution is its' value
-        double currentEnergy = currentSolution;
-        // Start annealing
-        while (temperature > coolingRate) {
-            // Get random position in our universe
-            int newSolutionPosition = this.getRandomIndex();
-            // Retrieve new solution's value
-            final double newSolutionEnergy = this.universe.get(newSolutionPosition);
+    return currentSolution;
+  }
 
-            // Check whether new solution should be accepted
-            if (this.acceptanceProbability(currentEnergy, newSolutionEnergy, temperature) > Math.random()) {
-                // Move on to the new solution
-                currentSolution = this.universe.get(newSolutionPosition);
-            }
-
-            // Compare to the global optima
-            if (newSolutionEnergy < bestSolution) {
-                // Mark as global optima (best found) if current global optima is greater
-                bestSolution = currentSolution;
-            }
-
-            // Cool down
-            temperature *= 1 - coolingRate;
-        }
-
-        return currentSolution;
+  private double acceptanceProbability(final double energy, final double newEnergy, final double temperature) {
+    if (newEnergy < energy) {
+      return 1;
     }
 
-    private double acceptanceProbability(final double energy, final double newEnergy, final double temperature) {
-        if (newEnergy < energy) {
-            return 1;
-        }
+    return Math.exp((energy - newEnergy) / temperature);
+  }
 
-        return Math.exp((energy - newEnergy) / temperature);
-    }
+  private int getRandomIndex() {
+    final Random random = new Random();
 
-    private int getRandomIndex() {
-        final Random random = new Random();
-
-        return random.nextInt(this.universe.size());
-    }
+    return random.nextInt(this.universe.size());
+  }
 }
